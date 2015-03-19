45d4b6efcbb55ad30d3fcb0afdd3105157984b55
Praveen Chakravarthula <praveen.chakravarthula@gmail.com>
2015-03-19 22:51:16 +0000

    TwoSum done

---
+++ src/me/pravc/leetcode/TwoSum.java
@@ -0,0 +1,43 @@
+package me.pravc.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, find two numbers such that they add up to a
+ * specific target number.
+ * 
+ * The function twoSum should return indices of the two numbers such that they
+ * add up to the target, where index1 must be less than index2. Please note that
+ * your returned answers (both index1 and index2) are not zero-based.
+ * 
+ * You may assume that each input would have exactly one solution.
+ * 
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ * 
+ * @author praveen
+ *
+ */
+public class TwoSum {
+
+	public static int[] findIndexesForSum(int[] numbers, int target) {
+		int[] output = new int[2];
+		output[0] = output[1] = 0;
+		Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			if (numMap.containsKey(target - numbers[i])) {
+				output[0] = numMap.get(target - numbers[i]);
+				output[1] = i + 1;
+				return output;
+			} else {
+				numMap.put(numbers[i], i + 1);
+			}
+
+		}
+
+		// not found
+		return output;
+	}
+
+}
---
+++ test/me/pravc/leetcode/TwoSumTest.java
@@ -0,0 +1,52 @@
+package me.pravc.leetcode;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+
+import org.testng.annotations.Test;
+
+public class TwoSumTest {
+
+	@Test
+	public void exampleTest() {
+		int[] input = new int[] { 2, 7, 11, 15 };
+		int target = 9;
+		
+		int[] output = TwoSum.findIndexesForSum(input, target);
+
+		assertNotNull(output);
+		assertEquals(output.length, 2);
+
+		assertEquals(output[0], 1);
+		assertEquals(output[1], 2);
+
+	}
+	
+	@Test
+	public void oneTtest() {
+		int[] input = new int[] { 9, 7, 5, 3, 2, 1, 10};
+		int target = 6;
+		
+		int[] output = TwoSum.findIndexesForSum(input, target);
+
+		assertNotNull(output);
+		assertEquals(output.length, 2);
+		
+		assertEquals(output[0], 3);
+		assertEquals(output[1], 6);
+	}
+	
+	@Test
+	public void negativeNumbersTest() {
+		int[] input = new int[] { 10, -1, -2, 300, 400, 60, 50, 2};
+		int target = 48;
+		
+		int[] output = TwoSum.findIndexesForSum(input, target);
+
+		assertNotNull(output);
+		assertEquals(output.length, 2);
+		
+		assertEquals(output[0], 3);
+		assertEquals(output[1], 7);
+	}
+}
